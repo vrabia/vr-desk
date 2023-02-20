071b5c474f32cee94c15f72237eeedb33f13171d
Arcalean Maria Lorena <arcalean.lorena22@gmail.com>
2023-02-21 00:46:24 +0200

    Made electron start with angular

---
+++ app.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+const url = require("url");
+const path = require("path");
+
+let appWindow;
+
+function initWindow() {
+  appWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      // nodeIntegration: true
+    }
+  });
+
+  appWindow.loadFile('dist/index.html');
+  appWindow.webContents.openDevTools();
+  appWindow.on('closed', function () {
+    appWindow = null
+  });
+}
+
+app.on('ready', initWindow);
+app.on('window-all-closed', function () {
+  // On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+app.on('activate', function () {
+  if (appWindow === null) {
+    initWindow()
+  }
+})
+
